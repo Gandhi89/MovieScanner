f4919f871a1e54a677f5e36ae7dad391aa6b698a
Shivam <shivamgandhi95@gmail.com>
2023-04-03 18:35:14 -0400

    Reused MovieList to display Favourites

--- src/screens/Favourite.tsx
+++ src/screens/Favourite.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import MovieList from '../components/MovieList';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 export const Favourite = () => {
+    const favorites = useSelector((state: RootState) => state.movies.favoriteMovies);
     return (
-        <View>
-            <Text>Movie</Text>
+        <View style={styles.root}>
+            <MovieList movies={favorites} />
         </View>
     );
 }
+const styles = StyleSheet.create({
+    headerButton: {
+        marginRight: 15
+    },
+    root: {
+        flex: 1
+    }
+})
